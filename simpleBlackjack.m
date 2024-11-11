4bb82aea9a98a8d5e83036601e17d9b4498065d6
yazanramadann <yazanfiras243@gmail.com>
2024-11-10 18:01:01 -0600

    Update simpleBlackjack.m

--- simpleBlackjack.m
+++ simpleBlackjack.m
@@ -13,3 +13,27 @@ function avgScore = simpleBlackjack(limit, trials)
 % OUTPUT avgScore – The average final score of
 % all hands.
 
+function avgScore = simpleBlackjack(limit, trials)
+    % Set default values if arguments are not provided
+    if nargin < 1
+        limit = 17; % Default limit
+    end
+    if nargin < 2
+        trials = 1000; % Default number of trials
+    end
+    
+    % Define card values
+    cards = [2 3 4 5 6 7 8 9 10 10 10 10];
+    totalScore = 0;
+    
+    for t = 1:trials
+        handTotal = 0;
+        while handTotal < limit
+            card = cards(randi(12)); % Draw a random card
+            handTotal = handTotal + card;
+        end
+        totalScore = totalScore + handTotal;
+    end
+    
+    avgScore = totalScore / trials;
+end
